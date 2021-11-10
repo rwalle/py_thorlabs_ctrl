83afc8a9eb178e293380f90660d2a1f6759e4297
Probst <mprobst6@gatech.edu>
2021-11-10 13:09:26 -0500

    added range limits

--- py_thorlabs_ctrl/kinesis/motor.py
+++ py_thorlabs_ctrl/kinesis/motor.py
@@ -32,9 +32,11 @@ class Motor:
     INIT_TIMEOUT = 5000
     max_velocity = Decimal(2.2)
     max_acceleration = Decimal(1.0)
+    buffer = 0.5 # mm
 
-    def __init__(self, serial_number):
+    def __init__(self, serial_number,motor_type):
         self.serial_number = str(serial_number)
+        self.motor_type = str(motor_type)
 
     def create(self):
         # abstract
@@ -78,15 +80,6 @@ class Motor:
         device = self.get_device()
         return Decimal.ToDouble(device.DevicePosition)
 
-    def get_max_velocity_du(self):
-        device = self.get_device()
-        params = device.GetVelocityParams_DeviceUnit()
-        return params.MaxVelocity
-
-    def get_position_du(self):
-        device = self.get_device()
-        return device.GetPositionCounter()
-
     def get_velocity(self):
         device = self.get_device()
         params = device.GetVelocityParams()
@@ -111,57 +104,84 @@ class Motor:
         device = self.get_device()
         device.Stop(0)
 
+    def max_range(self):
+        if self.motor_type == "translation":
+            return 25
+        elif self.motor_type == "rotation":
+            return 12
+        else:
+            print("not a valid motor type")
+
     def stop_immediate(self):
         device = self.get_device()
         device.StopImmediate()
 
-    def velocity(self,vel):
+    def velocity(self,vel,buffer=buffer):
         device = self.get_device()
         try:
             if vel < 0:
-                device.MoveContinuousAtVelocity(MotorDirection.Backward,abs(vel))
+                if self.get_position() < self.buffer:
+                    print("velocity negative")
+                    print("position: {}, buffer: {}".format(self.get_position(),self.buffer))
+                    print("Reached the bottom edge of the range")
+                    self.stop_immediate()
+                else:
+                    device.MoveContinuousAtVelocity(MotorDirection.Backward,abs(vel))
             else:
-                device.MoveContinuousAtVelocity(MotorDirection.Forward,abs(vel))
+                if self.get_position() > self.max_range()-self.buffer:
+                    print("velocity positive")
+                    print("position: {}, buffer: {}".format(self.get_position(),self.buffer))
+                    print("Reached the top edge of the range")
+                    self.stop_immediate()
+                else:
+                    device.MoveContinuousAtVelocity(MotorDirection.Forward,abs(vel))
         except Exception as e:
             print(e)
-### Move statements that pause the code until the previous statement has exectued then they execute ###
-#     def move_relative(self,dis):
-#         temp = True
-#         device = self.get_device()
-#         device.SetMoveRelativeDistance(Decimal(dis))
-#         while temp == True:
-#            itry:
-#                 device.MoveRelative(0)
-#                 temp = False
-#                 print(temp)
-#             except Exception as e:
-#                 print(e)
-# 
-#     def move_absolute(self,pos):
-#         device = self.get_device()
-#         temp = True
-#         while temp == True:
-#             try:
-#                 device.MoveTo(Decimal(pos),0)
-#                 temp = False
-#             except Exception as e:
-#                 print(e)
-# 
+ 
     def move_relative(self,dis):
         device = self.get_device()
         device.StopImmediate()
         device.SetMoveRelativeDistance(Decimal(dis))
         time.sleep(ENABLE_SLEEP_TIME)
+        print("max range: {}, min range: {}".format(self.max_range()-self.buffer,self.buffer))
+        print("requested position: {}".format(self.get_position()+dis))
         device.SetVelocityParams(self.max_velocity,self.max_acceleration)
-        device.MoveRelative(0)
-
+        if self.get_position()+dis > self.max_range()-self.buffer:
+            print("Move would result in invalid position")
+        elif self.get_position()+dis < self.buffer:
+            print("Move would result in invalid position")
+        else:
+            try:
+                device.MoveRelative(0)
+            except Exception as e:
+                print(e)
+            
     def move_absolute(self,pos):
         device = self.get_device()
         device.StopImmediate()
         params = device.GetVelocityParams()
         device.SetVelocityParams(self.max_velocity,self.max_acceleration)
         time.sleep(ENABLE_SLEEP_TIME)
-        device.MoveTo(Decimal(pos),0)
+        if (self.buffer < pos) and (pos < self.max_range()-self.buffer):
+            try:
+                device.MoveTo(Decmial(pos),0)
+            except Exception as e:
+                print(e)
+        else:
+            print("Move would result in invalid position")  
+
+    def reset(self):
+        device = self.get_device()
+        try:
+            self.disable()
+        except Exception as e:
+            print(e)
+        try: 
+            self.disconnect()
+        except Exception as e:
+            print(e)
+        self.create() # these should happen every time, if there was an error or not
+        self.enable()
 
     def get_status(self):
         device = self.get_device()
